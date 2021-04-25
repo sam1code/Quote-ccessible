0d55bae69c22e30d8579af99833e3155c192d5c1
sam <samcse95@gmail.com>
2021-04-25 13:56:28 +0530

    logo added

---
+++ frontend/src/components/Dashbord/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+import './style.css'
+
+
+function index() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default index
